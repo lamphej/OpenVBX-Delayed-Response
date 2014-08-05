f7013fbf3c34f2b4ce1b05cdbb688ca13a41c1c1
James Lamphere <jim@gmh-enterprises.com>
2014-08-04 22:20:18 -0400

    Now uses Twilio API and sends ALL the responses.

--- delay-response/applets/delay/twiml.php
+++ delay-response/applets/delay/twiml.php
@@ -1,12 +1,15 @@
 <?php
+include('/public_html/sms/OpenVBX/libraries/Services/Twilio.php');
+
+$sid = "AC5647f7bbee5e19c7ea681c4099cffd3c";
+$token = "82bb70cc653ce64bdf30176a0f218c8a";
+$from = $_REQUEST["From"];
+$client = new Services_Twilio($sid, $token);
+
 $delays = AppletInstance::getValue('delays[]');
 $messages = AppletInstance::getValue('messages[]');
 
 foreach ($delays AS $i=>$pause) {
     sleep(intval($pause));
-    $response = new TwimlResponse();
-    $response->message($messages[$i]);
-    $response->respond();
-    unset($response);
-    break;
+    $client->account->messages->sendMessage("+16467599725", $from, $messages[$i]);
 }
